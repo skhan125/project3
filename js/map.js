464f69267cdf5a0c54d54af7004b4f8aaf91f5f6
skhan125 <112320594+skhan125@users.noreply.github.com>
2022-11-22 23:54:42 -0600

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,15 +1,14 @@
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 42.058890, lng: -87.749071 },
-    zoom: 12,
+    center: { lat: 41.880193, lng: -87.633866 },
+    zoom: 15,
   });
 
   new google.maps.Marker({
-    position: { lat: 41.8349, lng: -87.62708876535238 },
+    position: { lat: 41.8349, lng: -87.6270 },
     map,
     title: "IIT!",
   });
-  
   new google.maps.Marker({
     position: { lat: 41.9142, lng: -88.3087 },
     map,
